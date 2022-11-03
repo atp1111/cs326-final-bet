52b102bb1b64555ae461047c437027c5a7e9595d
Chuchu <cuikoro@umass.edu>
2022-11-03 16:46:21 -0400

    Examples

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,7 @@
 //API requests for the home screen are handled here
 
+//Fetch listings from src index.jsm then create new div elements for each one 
+
 // listings = fetch("/listings") get request to get all listings from backend -> []
  //listingsDiv = document.getElementById("listings")
  // for each listing in listings, create HTML template string for a listing
--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ const port = 8000;
 
 app.use("/", express.static("./src/client/")); 
 
+//Ex. Get all listings from JSON array of list objects (Home)
+
 // app.get("/listings", (req, res) => {
 //   res.send([listings])
     // example listing object: {image, description, name}
 // })
 
+//Ex. Get all listings saved under a user (Manage Listings/ Saved Listings)
 //app.get("/listings/:userId", (req, res) => {
   // {userId} = req.params
 // })
