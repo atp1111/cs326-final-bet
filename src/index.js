7c08bd39113c19e06152bbcef8feb3c470a1aa11
Chuchu <cuikoro@umass.edu>
2022-11-20 18:41:36 -0500

    SQL

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import express from "express"
+import mysql from "mysql";
 
 //Backend Server which retrieves info
 
 //Start server
 const app = express();
 const port = process.env.PORT || 8000;
+
+var pool = mysql.createConnection({
+  host: "ec2-54-204-56-171.compute-1.amazonaws.com",
+  user: "mkhtopjnugvikz",
+  password: "13684de6d531b341061eee9bcc227e1ef8d183df1014c836153d9df531b3240c",
+  database: "d7g7bo1rdppcn1",
+  port: "5432"
+});
+
+await pool.connect(function(err) {
+  console.log("Connected!");
+});
 
 //Get all listings from JSON array of list and user objects (Home)
 let listings = [
@@ -48,16 +61,44 @@ let users = [
 app.use("/", express.static("./src/client/")); 
 app.use("/profile", express.static("./src/client/profile"));
 
-app.get("/listings", (req, res) => {
+/*app.get("/listings", (req, res) => {
   res.send([listings])
   //console.log(listings);
    // example listing object: {image, description, name}
-});
+});*/
+
+app.get('/listings', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    console.log(pool);
+    const result = await client.query('SELECT * FROM Listings'); 
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/listings', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
  app.get("/users", (req, res) => {
   res.send([users])
   console.log(users);
 });
+
+//TODO: Temp Database Table, Edit later/ Adapt for Listings/Users
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table'); //change sql query
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 //Ex. Get all listings saved under a user (Manage Listings/ Saved Listings)
 //app.get("/listings/:userId", (req, res) => {
